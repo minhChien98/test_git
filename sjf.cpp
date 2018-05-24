c2433b73cb812259aba6d4f29e7781f65c9485eb
minhChien98 <chiendev98@gmail.com>
2018-05-24 12:16:46 +0700

    add source

---
+++ sjf.cpp
@@ -0,0 +1,24 @@
+	#include<iostream>
+#include<iomanip>
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+#define MAX 20
+
+struct process{
+	int pro;
+	int cpuB;
+	int timeXh;
+};
+
+struct time{
+    int timeWait;
+    int timePro;
+};
+
+int main()
+{
+	return 0;
+}
