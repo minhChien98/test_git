1dc05629869e6080e5893c7e39866ad22a75415d
minhChien98 <chiendev98@gmail.com>
2018-05-24 12:25:55 +0700

    add lai nhap

--- sjf.cpp
+++ sjf.cpp
@@ -18,6 +18,22 @@ struct timee{
     int timePro;
 };
 
+void nhap(process test[],int &n)
+{
+	cout << "Nhap so tien trinh: ";
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		cout << "\tNhap tien trinh " << i+1 << endl;
+		cout << "\t\tNhap ten tien trinh: ";
+		fflush(stdin);gets(test[i].pro);
+		cout << "\t\tNhap thoi gian xuat hien: ";
+		cin >> test[i].timeXh;
+		cout << "\t\tNhap thoi gian xu li: ";
+		cin >> test[i].cpuB;
+	}
+}
+
 int main()
 {
 	process prox[MAX];
